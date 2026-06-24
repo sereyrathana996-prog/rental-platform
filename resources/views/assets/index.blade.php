c9dd89e89c7ff4f062fe184f4a4f3fe143026401
sereyrathana996-prog <sereyrathana996@gmail.com>
2026-06-24 23:30:42 +0700

    Write asset card as components.

--- resources/views/assets/index.blade.php
+++ resources/views/assets/index.blade.php
@@ -82,78 +82,22 @@
 
     @forelse ($assets as $asset)
 
-        <div
-            class="
-                bg-white
-                rounded-xl
-                shadow
-                overflow-hidden
-            "
-        >
-
-            @if ($asset->cover_photo)
-
-                <img
-                    src="{{ asset('storage/' . $asset->cover_photo) }}"
-                    class="w-full h-60 object-cover"
-                >
-
-            @endif
-
-
-            <div class="p-5">
-
-                <h2 class="text-xl font-bold">
-                    {{ $asset->title }}
-                </h2>
-
-                <p class="text-green-600 text-lg mt-2">
-                    ${{ $asset->price_per_day }}
-                </p>
-
-
-                <div class="flex gap-3 mt-5">
-
-                    <a
-                        href="{{ route('assets.show', $asset->id) }}"
-                        class="
-                            bg-blue-500
-                            text-white
-                            px-4
-                            py-2
-                            rounded
-                        "
-                    >
-                        View
-                    </a>
-
-
-                    <a
-                        href="{{ route('bookings.create', $asset->id) }}"
-                        class="
-                            bg-green-500
-                            text-white
-                            px-4
-                            py-2
-                            rounded
-                        "
-                    >
-                        Book
-                    </a>
-
-                </div>
-
-            </div>
-
-        </div>
+    ```
+    <x-asset-card
+        :asset="$asset"
+    />
+    ```
 
     @empty
 
-        <p class="text-gray-500">
-            No assets found
-        </p>
+    <p class="text-gray-500">
+
+    No assets found
+
+    </p>
 
     @endforelse
+
 
 </div>
 ```
