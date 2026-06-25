732f4525b3ce30fc97894b416bc2029503436d75
sereyrathana996-prog <sereyrathana996@gmail.com>
2026-06-25 14:04:11 +0700

    Add tailwind style to View asset detail page.

--- resources/views/assets/show.blade.php
+++ resources/views/assets/show.blade.php
@@ -1,73 +1,156 @@
-<h1>
+<x-app-layout>
+
+<div class="max-w-6xl mx-auto p-8">
+
+<div
+class="
+grid
+grid-cols-1
+lg:grid-cols-2
+gap-10
+">
+
+<div>
+
+@if($asset->cover_photo)
+
+<img
+src="{{ asset('storage/'.$asset->cover_photo) }}"
+class="
+w-full
+rounded-2xl
+shadow
+h-[500px]
+object-cover
+">
+
+@endif
+
+</div>
+
+<div>
+
+<h1
+class="
+text-5xl
+font-bold
+"
+>
 
 {{ $asset->title }}
 
 </h1>
 
-@if($asset->cover_photo)
-
-<img
-src="{{ Storage::url(
-$asset->cover_photo
-) }}"
-width="300">
-
-@endif
-
-
-<p>
-
-Description:
+<p
+class="
+text-gray-600
+mt-6
+"
+>
 
 {{ $asset->description }}
 
 </p>
 
+<div
+class="
+mt-8
+"
+>
 
-<p>
+<p
+class="
+text-4xl
+font-bold
+text-blue-600
+"
+>
 
-Price:
+${{ $asset->price_per_day }}
 
-$
+<span
+class="
+text-lg
+text-gray-500
+"
 
-{{ $asset->price_per_day }}
+>
+
+/ day
+
+</span>
 
 </p>
 
+</div>
 
-<p>
+<div
+class="
+mt-6
+"
+>
 
-Deposit:
+<span
+class="
+bg-green-100
+text-green-700
+px-4
+py-2
+rounded-full
+"
 
-$
-
-{{ $asset->deposit_amount }}
-
-</p>
-
-
-<p>
-
-Status:
+>
 
 {{ $asset->status }}
 
-</p>
+</span>
 
-<hr>
+</div>
 
-<a href="{{ route('bookings.create', $asset->id) }}">
+<div
+class="
+mt-10
+flex
+gap-4
+">
+
+<a
+href="{{ route('bookings.create',$asset->id) }}"
+class="
+bg-blue-600
+text-white
+px-6
+py-4
+rounded-xl
+"
+
+>
 
 Book Now
 
 </a>
 
-
 <a
-href="{{ route(
-'assets.index'
-) }}">
+href="{{ route('assets.index') }}"
+class="
+bg-gray-200
+px-6
+py-4
+rounded-xl
+"
+
+>
 
 Back
 
 </a>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</x-app-layout>
